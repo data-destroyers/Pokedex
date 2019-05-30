b0fbd03bb4749c8c83e2d6280ccd493cac36a7b9
Erin Gurley <erinmgurley@gmail.com>
2019-05-30 16:21:41 -0700

    added paging and loading component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import Component from './Component.js';
 import Header from './Header.js';
 import PokemonList from './PokemonList.js';
+import Loading from './Loading.js';
 import pokedexApi from '../services/pokedex-api.js';
+import Paging from './Paging.js';
 
 class App extends Component {
 
@@ -12,6 +14,9 @@ class App extends Component {
         const header = new Header();
         dom.insertBefore(header.render(), main);
 
+        const loading = new Loading({ loading: true });
+        main.appendChild(loading.render());
+
         const pokemonList = new PokemonList({ pokemon: [] });
         main.appendChild(pokemonList.render());
 
@@ -19,6 +24,9 @@ class App extends Component {
             .then(response => {
                 const pokemon = response.results;
                 pokemonList.update({ pokemon });
+            })
+            .finally(() => {
+                loading.update({ loading: false });
             });
 
         return dom;
