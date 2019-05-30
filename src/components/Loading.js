3d42bb6b1b6cf978bec4cc04410288f47fbde792
Erin Gurley <erinmgurley@gmail.com>
2019-05-30 15:01:02 -0700

    created loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,21 @@
+import Component from './Component.js';
+
+class Loading extends Component {
+
+    renderTemplate() {
+
+        const loading = this.props.loading;
+        
+        if(!loading) {
+            return '<div></div>';
+        }
+
+        return /*html*/`
+            <div>
+                <img src="../../assets/loading.gif">
+            </div>
+        `;
+    }
+}
+
+export default Loading;
