101403cb4718c02773dff45b0b09cc17c7754933
Erin Gurley <erinmgurley@gmail.com>
2019-05-30 16:20:41 -0700

    create pokemon item for each pokemon

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,22 +1,22 @@
 import Component from './Component.js';
+import PokemonItem from './PokemonItem.js';
 
 class PokemonList extends Component {
+
+    render() {
+        const list = this.renderDOM();
+        this.props.pokemon.forEach(pokemon => {
+            const pokemonItem = new PokemonItem({ pokemon });
+            list.appendChild(pokemonItem.render());
+
+        });
+
+        return list;
+    }
 
     renderTemplate() {
         return /*html*/`
             <ul class="pokemon-list">
-                <li class="pokemon-item">
-                    <h2>Pichu</h2>
-                    <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png">
-                </li>
-                <li class="pokemon-item">
-                    <h2>Pichu</h2>
-                    <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png">
-                </li>
-                <li class="pokemon-item">
-                    <h2>Pichu</h2>
-                    <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png">
-                </li>
             </ul>
         `;
     }
