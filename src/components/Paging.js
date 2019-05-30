0e1c3e2156173b3323ed95e36efdc34f53b61efb
Erin Gurley <erinmgurley@gmail.com>
2019-05-30 16:22:14 -0700

    added paging component

---
+++ src/components/Paging.js
@@ -0,0 +1,12 @@
+import Component from './Component.js';
+
+class Paging extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            
+        `;
+    }
+}
+
+export default Paging;
