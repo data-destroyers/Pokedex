3e696c40701c148000491423dd6902af043a8185
Jonathan Forney <jonathan.forney@gmail.com>
2019-05-31 11:30:11 -0700

    Created sort component

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,16 +1,33 @@
 import Component from './Component.js';
+import hashStorage from '../hash-storage.js';
 
 class Sort extends Component {
+    render() {
+        const form = this.renderDOM();
+
+        form.addEventListener('input', () => {
+            const formData = new FormData(form);
+
+            const sortOptions = {
+                sort: formData.get('sort'),
+                direction: formData.get('direction')
+            };
+            hashStorage.set(sortOptions);
+        });
+
+        return form;
+    }
+    
     renderTemplate() {
         return /*html*/`
         <form class="sort-form">
-            <select name="property">
+            <select name="sort">
                 <option value="height">Height</option>
                 <option value="weight">Weight</option>
             </select>
-            <select name="property">
-                <option value="1">Ascending</option>
-                <option value="-1">Descending</option>
+            <select name="direction">
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
             </select>
         </form>
     `;
