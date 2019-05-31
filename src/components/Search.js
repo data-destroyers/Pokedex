878b11bcbf7dbdebfb2d24fed86cf026694abb42
Jonathan Forney <jonathan.forney@gmail.com>
2019-05-31 09:35:18 -0700

    added search function and connected to hash

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,32 @@
 import Component from './Component.js';
 import hashStorage from '../hash-storage.js';
-import QUERY from '../QUERY.js';
 
 class Search extends Component {
     render() {
         const dom = this.renderDOM();
-        
         const input = dom.querySelector('input');
 
         dom.addEventListener('submit', event => {
             event.preventDefault();
             const search = input.value;
-            hashStorage.set({ 'pokemon': search });
+            hashStorage.set({ 'pokemon': search, page: 1 });
         });
 
+        function setInputFromHash() {
+            const queryProps = hashStorage.get();
+            input.value = queryProps.search;
+            if(queryProps.search === undefined) {
+                input.value = '';
+            }
+            else {
+                input.value = queryProps.search;
+            }
+        }
+        setInputFromHash();
+
+        window.addEventListener('hashchange', () => {
+            setInputFromHash();
+        });
         return dom;
     }
     renderTemplate() {
